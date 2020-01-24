6dffe5b160b3516f643c583f88a32ee285a1722b
Wolfgang Brandenburger <wolfgang.brandenburger@live.de>
2020-01-24 20:22:47 +0100

    changes in setup.py

--- expmgmt/__init__.py
+++ expmgmt/__init__.py
@@ -8,22 +8,22 @@ __email__ = "wolfgang.brandenburger@outlook.com"
 
 
 # if os.environ.get("EXPMGMT_DEBUG"):
-import logging
-import colorama
+# import logging
+# import colorama
 
-colorama.init()
+# colorama.init()
 
-log_format = (
-    'File "%(pathname)s", line %(lineno)s:\n' +
-    colorama.Fore.YELLOW +
-    '%(levelname)s' +
-    ':' +
-    colorama.Fore.GREEN +
-    '%(name)s' +
-    colorama.Fore.CYAN +
-    ':' +
-    '%(message)s' +
-    colorama.Style.RESET_ALL
-)
+# log_format = (
+#     'File "%(pathname)s", line %(lineno)s:\n' +
+#     colorama.Fore.YELLOW +
+#     '%(levelname)s' +
+#     ':' +
+#     colorama.Fore.GREEN +
+#     '%(name)s' +
+#     colorama.Fore.CYAN +
+#     ':' +
+#     '%(message)s' +
+#     colorama.Style.RESET_ALL
+# )
 
-logging.basicConfig(format=log_format, level=logging.DEBUG)
+# logging.basicConfig(format=log_format, level=logging.DEBUG)
