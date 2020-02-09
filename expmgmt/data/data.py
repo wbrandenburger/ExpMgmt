d5f3607735047183d19bf486d83425a06e7cc80b
Wolfgang Brandenburger <wolfgang.brandenburger@live.de>
2020-02-09 23:02:50 +0100

    fixed some bugs

--- expmgmt/data/data.py
+++ expmgmt/data/data.py
@@ -2,6 +2,9 @@
 #   data.py -----------------------------------------------------------------
 # ===========================================================================
 
+import re
+import os
+
 #   function ----------------------------------------------------------------
 # ---------------------------------------------------------------------------
 def get_data_tensor_intervall(data_tensor, start, end):
@@ -15,14 +18,30 @@ def get_sub_data_tensor(data_tensor, index_list, sort=False):
 
     for count, item_tensor in enumerate(data_tensor): 
         data_tensor[count] = [data_tensor[count][i] for i in index_list ]
+
     return data_tensor
 
 #   function ----------------------------------------------------------------
 # ---------------------------------------------------------------------------
-def validate_data_tensor(data_tensor, index_list, sort=False):
-    if sort:
-        index_list.sort()
+def get_data_tensor_regex(data_tensor, regex, group):
 
-    for count, item_tensor in enumerate(data_tensor):
-        data_tensor[count] = [data_tensor[count][i] for i in index_list ]
-    return data_tensor
+    regex = regex if isinstance(regex, list) else [regex]
+    group = group if isinstance(regex, list) else [group]
+
+    data_tensor_dict = dict()
+    for item_tensor in data_tensor: 
+        item = os.path.basename(item_tensor[0])
+        for r, g in zip(regex, group):
+            if item:
+                res = re.compile(r)
+                result = res.match(item)
+                item = result.group(g) if result else None
+                if not item in data_tensor_dict.keys():
+                    data_tensor_dict[item] = item_tensor
+
+    return [ v for v in data_tensor_dict.values() ]
+
+#   function ----------------------------------------------------------------
+# ---------------------------------------------------------------------------
+def validate_data_tensor(data_tensor, index_list, sort=False):
+    return True
