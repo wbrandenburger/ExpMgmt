6e0602b29e240dabbecc938ff929e19f3f15e23b
Wolfgang Brandenburger <wolfgang.brandenburger@live.de>
2020-02-10 17:14:46 +0100

    fixed bug

--- expmgmt/config/settings.py
+++ expmgmt/config/settings.py
@@ -423,6 +423,7 @@ def get_dataset_settings_dir(dataset):
 def get_dataset_meta_settings(dataset, setting):
     if not _DATASET:
         set_dataset_config()
+        
     meta_settings = get_dataset_setting(dataset)["default"]["meta"]
     meta_settings.update(get_dataset_setting(dataset)[setting]["meta"])
     return meta_settings
